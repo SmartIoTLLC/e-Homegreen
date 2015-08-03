7583064e1c01ef177ba3164a1e35d63e9e1f4da9
vladimirzivanov <vrza88@gmail.com>
2015-08-03 10:49:20 +0200

    update

--- e-Homegreen/SettingsViewController.swift
+++ e-Homegreen/SettingsViewController.swift
@@ -28,13 +28,40 @@ class SettingsViewController: CommonViewController, UITableViewDelegate, UITable
         // Dispose of any resources that can be recreated.
     }
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return settingArray.count
     }
+    
+    func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 3
+    }
+    
+    func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 3
+    }
+    
+    func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        var headerView:UIView = UIView(frame: CGRectMake(0, 0, 1024, 3))
+        headerView.backgroundColor = UIColor.clearColor()
+        return headerView
+    }
+    
+    func tableView(tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        var footerView:UIView = UIView(frame: CGRectMake(0, 0, 1024, 3))
+        footerView.backgroundColor = UIColor.clearColor()
+        return footerView
+    }
+    
+    
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         if let cell = tableView.dequeueReusableCellWithIdentifier("settingsCell") as? SettinsTableViewCell {
 //            cell.selectionStyle = UITableViewCellSelectionStyle.None
-            cell.tableCellTitle.text = settingArray[indexPath.row]
+            cell.tableCellTitle.text = settingArray[indexPath.section]
+            cell.layer.cornerRadius = 5
             return cell
             
         }
@@ -44,17 +71,17 @@ class SettingsViewController: CommonViewController, UITableViewDelegate, UITable
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        if indexPath.row == 0 {
+        if indexPath.section == 0 {
             dispatch_async(dispatch_get_main_queue(),{
                 self.performSegueWithIdentifier("menuSettings", sender: self)
             })
         }
-        if indexPath.row == 1 {
+        if indexPath.section == 1 {
             dispatch_async(dispatch_get_main_queue(),{
                 self.performSegueWithIdentifier("databaseSettings", sender: self)
             })
         }
-        if indexPath.row == 2 {
+        if indexPath.section == 2 {
             dispatch_async(dispatch_get_main_queue(),{
                 self.performSegueWithIdentifier("connectionSettings", sender: self)
 //                self.showCellParametar()
