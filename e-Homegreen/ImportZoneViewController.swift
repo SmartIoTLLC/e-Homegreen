0cbed7c4a510ebbbea374d5dc815c7b8bc241535
teodorstevic <teodorstevic@Sladjans-Mac.local>
2015-09-19 11:53:45 +0200

    Update

---
+++ e-Homegreen/ImportZoneViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ImportZoneViewController.swift
+//  e-Homegreen
+//
+//  Created by Teodor Stevic on 9/19/15.
+//  Copyright © 2015 Teodor Stevic. All rights reserved.
+//
+
+import UIKit
+
+class ImportZoneViewController: UIViewController {
+
+    var zones:[Zone] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+extension ImportZoneViewController: UITableViewDelegate {
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+}
+extension ImportZoneViewController: UITableViewDataSource {
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return zones.count
+    }
+}
+class ImportZoneTableViewCell: UITableViewCell {
+    
+}
