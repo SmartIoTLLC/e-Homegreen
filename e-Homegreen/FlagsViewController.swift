de683a0df640a3bdd769452d727c065d82d65d21
teodorstevic <teodorstevic@Sladjans-Mac.local>
2015-10-07 16:08:19 +0200

    Update

--- e-Homegreen/FlagsViewController.swift
+++ e-Homegreen/FlagsViewController.swift
@@ -7,18 +7,231 @@
 //
 
 import UIKit
+import CoreData
 
 class FlagsViewController: CommonViewController {
-
+    
+    var appDel:AppDelegate!
+    var flags:[Flag] = []
+    var error:NSError? = nil
+    
+    private var sectionInsets = UIEdgeInsets(top: 10, left: 5, bottom: 10, right: 5)
+    private let reuseIdentifier = "FlagCell"
+    var collectionViewCellSize = CGSize(width: 150, height: 180)
+    
+    @IBOutlet weak var flagsCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        //        cyclesTextField.delegate = self
+        
+        if self.view.frame.size.width == 414 || self.view.frame.size.height == 414 {
+            collectionViewCellSize = CGSize(width: 128, height: 156)
+        }else if self.view.frame.size.width == 375 || self.view.frame.size.height == 375 {
+            collectionViewCellSize = CGSize(width: 118, height: 144)
+        }
+        
+        appDel = UIApplication.sharedApplication().delegate as! AppDelegate
+        refreshFlagList()
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "refreshFlagList", name: "refreshFlagListNotification", object: nil)
         // Do any additional setup after loading the view.
     }
-
+    
+    override func viewWillLayoutSubviews() {
+        if UIDevice.currentDevice().orientation == UIDeviceOrientation.LandscapeLeft || UIDevice.currentDevice().orientation == UIDeviceOrientation.LandscapeRight {
+            if self.view.frame.size.width == 568{
+                sectionInsets = UIEdgeInsets(top: 5, left: 25, bottom: 5, right: 25)
+            }else if self.view.frame.size.width == 667{
+                sectionInsets = UIEdgeInsets(top: 5, left: 12, bottom: 5, right: 12)
+            }else{
+                sectionInsets = UIEdgeInsets(top: 5, left: 15, bottom: 5, right: 15)
+            }
+        }else{
+            if self.view.frame.size.width == 320{
+                sectionInsets = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
+            }else if self.view.frame.size.width == 375{
+                sectionInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+            }else{
+                sectionInsets = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
+            }
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    /*
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
+    }
+    */
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    func refreshFlagList () {
+        updateFlagsList()
+        flagsCollectionView.reloadData()
+    }
+    func updateFlagsList () {
+        let fetchRequest = NSFetchRequest(entityName: "Flag")
+        let sortDescriptorOne = NSSortDescriptor(key: "gateway.name", ascending: true)
+        let sortDescriptorTwo = NSSortDescriptor(key: "flagId", ascending: true)
+        let sortDescriptorThree = NSSortDescriptor(key: "flagName", ascending: true)
+        fetchRequest.sortDescriptors = [sortDescriptorOne, sortDescriptorTwo, sortDescriptorThree]
+        let predicate = NSPredicate(format: "gateway.turnedOn == %@", NSNumber(bool: true))
+        fetchRequest.predicate = predicate
+        do {
+            let fetResults = try appDel.managedObjectContext!.executeFetchRequest(fetchRequest) as? [Flag]
+            flags = fetResults!
+        } catch let error1 as NSError {
+            error = error1
+            print("Unresolved error \(error), \(error!.userInfo)")
+            abort()
+        }
+        
+    }
+    func saveChanges() {
+        do {
+            try appDel.managedObjectContext!.save()
+        } catch let error1 as NSError {
+            error = error1
+            print("Unresolved error \(error), \(error!.userInfo)")
+            abort()
+        }
+    }
+}
 
+extension FlagsViewController: UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        //        var address:[UInt8] = []
+        //        if sequences[indexPath.row].isBroadcast.boolValue {
+        //            address = [0xFF, 0xFF, 0xFF]
+        //        } else {
+        //            address = [UInt8(Int(sequences[indexPath.row].gateway.addressOne)), UInt8(Int(sequences[indexPath.row].gateway.addressTwo)), UInt8(Int(sequences[indexPath.row].address))]
+        //        }
+        //        if let cycles = sequences[indexPath.row].sequenceCycles as? Int {
+        //            if cycles >= 0 && cycles <= 255 {
+        //                SendingHandler.sendCommand(byteArray: Function.setSequence(address, id: Int(sequences[indexPath.row].sequenceId), cycle: UInt8(cycles)), gateway: sequences[indexPath.row].gateway)
+        //            }
+        //        } else {
+        //            SendingHandler.sendCommand(byteArray: Function.setSequence(address, id: Int(sequences[indexPath.row].sequenceId), cycle: 0x00), gateway: sequences[indexPath.row].gateway)
+        //        }
+        //        sequenceCollectionView.reloadData()
+    }
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAtIndex section: Int) -> UIEdgeInsets {
+        return sectionInsets
+    }
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        
+        return collectionViewCellSize
+        
+    }
+}
 
+extension FlagsViewController: UICollectionViewDataSource {
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return flags.count
+    }
+    
+    func openCellParametar (gestureRecognizer: UILongPressGestureRecognizer){
+        let tag = gestureRecognizer.view!.tag
+        if gestureRecognizer.state == UIGestureRecognizerState.Began {
+            let location = gestureRecognizer.locationInView(flagsCollectionView)
+            if let index = flagsCollectionView.indexPathForItemAtPoint(location){
+                let cell = flagsCollectionView.cellForItemAtIndexPath(index)
+//                showFlagParametar(CGPoint(x: cell!.center.x, y: cell!.center.y - flagsCollectionView.contentOffset.y), timer: flags[tag])
+            }
+        }
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! FlagCollectionViewCell
+        cell.flagTitle.text = "\(flags[indexPath.row].flagName)"
+        
+        let longPress:UILongPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: "openCellParametar:")
+        longPress.minimumPressDuration = 0.5
+        cell.flagTitle.userInteractionEnabled = true
+        cell.flagTitle.addGestureRecognizer(longPress)
+        
+        if let flagImage = UIImage(data: flags[indexPath.row].flagImageOne) {
+            cell.flagImageView.image = flagImage
+        }
+        
+        if let flagImage = UIImage(data: flags[indexPath.row].flagImageTwo) {
+            cell.flagImageView.highlightedImage = flagImage
+        }
+        
+        let tap:UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "buttonPressed:")
+        cell.flagButton.addGestureRecognizer(tap)
+        cell.flagButton.tag = indexPath.row
+        
+        cell.layer.cornerRadius = 5
+        cell.layer.borderColor = UIColor.grayColor().CGColor
+        cell.layer.borderWidth = 0.5
+        return cell
+    }
+    
+    func buttonPressed (gestureRecognizer:UITapGestureRecognizer) {
+        let tag = gestureRecognizer.view!.tag
+        if let flagId = flags[tag].flagId as? Int {
+            var address:[UInt8] = []
+            if flags[tag].isBroadcast.boolValue {
+                address = [0xFF, 0xFF, 0xFF]
+            } else {
+                address = [UInt8(Int(flags[tag].gateway.addressOne)), UInt8(Int(flags[tag].gateway.addressTwo)), UInt8(Int(flags[tag].address))]
+            }
+//            SendingHandler.sendCommand(byteArray: Function.setSequence(address, id: sequenceId, cycle: 0xEF), gateway: sequences[tag].gateway)
+            //        RepeatSendingHandler(byteArray: <#[UInt8]#>, gateway: <#Gateway#>, notificationName: <#String#>, device: <#Device#>, oldValue: <#Int#>)
+        }
+    }
+}
+class FlagCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var flagTitle: UILabel!
+    @IBOutlet weak var flagImageView: UIImageView!
+    @IBOutlet weak var flagButton: UIButton!
+    
+    override func drawRect(rect: CGRect) {
+        
+        let path = UIBezierPath(roundedRect: rect,
+            byRoundingCorners: UIRectCorner.AllCorners,
+            cornerRadii: CGSize(width: 5.0, height: 5.0))
+        path.addClip()
+        path.lineWidth = 2
+        
+        UIColor.lightGrayColor().setStroke()
+        
+        let context = UIGraphicsGetCurrentContext()
+        let colors = [UIColor(red: 13/255, green: 76/255, blue: 102/255, alpha: 1.0).colorWithAlphaComponent(0.95).CGColor, UIColor(red: 82/255, green: 181/255, blue: 219/255, alpha: 1.0).colorWithAlphaComponent(1.0).CGColor]
+        
+        
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        let colorLocations:[CGFloat] = [0.0, 1.0]
+        
+        let gradient = CGGradientCreateWithColors(colorSpace,
+            colors,
+            colorLocations)
+        
+        let startPoint = CGPoint.zero
+        let endPoint = CGPoint(x:0, y:self.bounds.height)
+        
+        CGContextDrawLinearGradient(context, gradient, startPoint, endPoint, CGGradientDrawingOptions(rawValue: 0))
+        
+        path.stroke()
+    }
+    
 }
