073434fb06082c7b55aeb0d82053042d57d10d02
Vladimir Zivanov <vrza88@gmail.com>
2016-03-09 15:11:55 +0100

    update pc controll in menu

---
+++ e-Homegreen/PCControlViewController.swift
@@ -0,0 +1,50 @@
+//
+//  PCControlViewController.swift
+//  e-Homegreen
+//
+//  Created by Vladimir Zivanov on 3/9/16.
+//  Copyright © 2016 Teodor Stevic. All rights reserved.
+//
+
+import UIKit
+
+class PCControlViewController: CommonViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    var collectionViewCellSize = CGSize(width: 150, height: 180)
+    private var sectionInsets = UIEdgeInsets(top: 0, left: 1, bottom: 0, right: 1)
+    
+    @IBOutlet weak var pccontrolCollectionView: UICollectionView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        pccontrolCollectionView.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: "collectionCell")
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCellWithReuseIdentifier("pccontrolCell", forIndexPath: indexPath) as? PCControlCell{
+            cell.setItem()
+            return cell
+        }
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("collectionCell", forIndexPath: indexPath)
+        return cell
+    }
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAtIndex section: Int) -> UIEdgeInsets {
+        return sectionInsets
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        return collectionViewCellSize
+    }
+    
+
+}
