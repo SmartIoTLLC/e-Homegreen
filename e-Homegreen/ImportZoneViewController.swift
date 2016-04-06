a14cc3219ae4ed21c46ac82fd8c707bbb9dd6bae
Marko Stajic <marko.stajic@gmail.com>
2016-04-06 12:43:57 +0200

    Drag & Drop table cells in zone list

--- e-Homegreen/ImportZoneViewController.swift
+++ e-Homegreen/ImportZoneViewController.swift
@@ -25,10 +25,120 @@ class ImportZoneViewController: UIViewController, ImportFilesDelegate, PopOverIn
         self.navigationController?.navigationBar.setBackgroundImage(imageLayerForGradientBackground(), forBarMetrics: UIBarMetrics.Default)
         
         appDel = UIApplication.sharedApplication().delegate as! AppDelegate
+        
+        let longpress = UILongPressGestureRecognizer(target: self, action: #selector(ImportZoneViewController.longPressGestureRecognized(_:)))
+        importZoneTableView.addGestureRecognizer(longpress)
 
         refreshZoneList()
         
         // Do any additional setup after loading the view.
+    }
+    
+    func longPressGestureRecognized(gestureRecognizer: UIGestureRecognizer){
+        
+        let longPress = gestureRecognizer as! UILongPressGestureRecognizer
+        let state = longPress.state
+        let locationInView = longPress.locationInView(importZoneTableView)
+        let indexPath = importZoneTableView.indexPathForRowAtPoint(locationInView)
+        
+        struct My {
+            static var cellSnapshot : UIView? = nil
+        }
+        struct Path {
+            static var initialIndexPath : NSIndexPath? = nil
+        }
+        
+        
+        
+        switch state {
+        case UIGestureRecognizerState.Began:
+            
+            if indexPath != nil {
+                
+                Path.initialIndexPath = indexPath
+                let cell = importZoneTableView.cellForRowAtIndexPath(indexPath!) as UITableViewCell!
+                My.cellSnapshot  = snapshopOfCell(cell)
+                var center = cell.center
+                
+                My.cellSnapshot!.center = center
+                My.cellSnapshot!.alpha = 0.0
+                importZoneTableView.addSubview(My.cellSnapshot!)
+                
+                
+                UIView.animateWithDuration(0.25, animations: { () -> Void in
+                    
+                    center.y = locationInView.y
+                    My.cellSnapshot!.center = center
+                    My.cellSnapshot!.transform = CGAffineTransformMakeScale(1.02, 1.02)
+                    My.cellSnapshot!.alpha = 0.98
+                    cell.alpha = 0.0
+                    
+                    }, completion: { (finished) -> Void in
+                        if finished {
+                            cell.hidden = true
+                        }
+                })
+            }
+            
+        case UIGestureRecognizerState.Changed:
+            var center = My.cellSnapshot!.center
+            
+            center.y = locationInView.y
+            
+            My.cellSnapshot!.center = center
+            
+            if ((indexPath != nil) && (indexPath != Path.initialIndexPath)) {
+// TODO:
+//                swap(&itemsArray[indexPath!.row], &itemsArray[Path.initialIndexPath!.row])
+                importZoneTableView.moveRowAtIndexPath(Path.initialIndexPath!, toIndexPath: indexPath!)
+                Path.initialIndexPath = indexPath
+                
+            }
+            
+        default:
+            let cell = importZoneTableView.cellForRowAtIndexPath(Path.initialIndexPath!) as! ImportZoneTableViewCell!
+            cell.hidden = false
+            cell.alpha = 0.0
+            
+            UIView.animateWithDuration(0.25, animations: { () -> Void in
+                
+                My.cellSnapshot!.center = cell.center
+                My.cellSnapshot!.transform = CGAffineTransformIdentity
+                My.cellSnapshot!.alpha = 0.0
+                
+                cell.alpha = 1.0
+                }, completion: { (finished) -> Void in
+                    
+                    if finished {
+                        
+                        Path.initialIndexPath = nil
+                        My.cellSnapshot!.removeFromSuperview()
+                        My.cellSnapshot = nil
+                        
+                    }
+                    
+            })
+            
+        }
+    }
+    
+    func snapshopOfCell(inputView: UIView) -> UIView {
+        UIGraphicsBeginImageContextWithOptions(inputView.bounds.size, false, 0.0)
+        
+        inputView.layer.renderInContext(UIGraphicsGetCurrentContext()!)
+        let image = UIGraphicsGetImageFromCurrentImageContext() as UIImage
+        
+        UIGraphicsEndImageContext()
+        
+        let cellSnapshot : UIView = UIImageView(image: image)
+        cellSnapshot.layer.masksToBounds = false
+        cellSnapshot.layer.cornerRadius = 0.0
+        cellSnapshot.layer.shadowOffset = CGSizeMake(-5.0, 0.0)
+        cellSnapshot.layer.shadowRadius = 5.0
+        cellSnapshot.layer.shadowOpacity = 0.4
+        
+        return cellSnapshot
+        
     }
 
     override func didReceiveMemoryWarning() {
