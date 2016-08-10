3ef9bfa16c85259d9ff832e7489f25973fa554db
Marko Stajic <marko.stajic@gmail.com>
2016-08-10 14:17:37 +0200

    Bug patch - No securities caused crash

--- e-Homegreen/IncomingHandler.swift
+++ e-Homegreen/IncomingHandler.swift
@@ -624,50 +624,52 @@ class IncomingHandler: NSObject {
     func securityFeedbackHandler (byteArray:[Byte]) {
         parseMessage(byteArray)
         fetchEntities("Security")
-        //FIXME: Pucalo je security zato sto nema u svim gatewayovima security        
-        let address = [Byte(Int(securities[0].addressOne)), Byte(Int(securities[0].addressTwo)), Byte(Int(securities[0].addressThree))]
-        if byteArray[2] == address[0] && byteArray[3] == address[1] && byteArray[4] == address[2] {
-            let defaults = NSUserDefaults.standardUserDefaults()
-            if byteArray[7] == 0x02 {
-                switch byteArray[8] {
-                case 0x00:
-                    defaults.setValue("Disarm", forKey: UserDefaults.Security.SecurityMode)
-                case 0x01:
-                    defaults.setValue("Away", forKey: UserDefaults.Security.SecurityMode)
-                case 0x02:
-                    defaults.setValue("Night", forKey: UserDefaults.Security.SecurityMode)
-                case 0x03:
-                    defaults.setValue("Day", forKey: UserDefaults.Security.SecurityMode)
-                case 0x04:
-                    defaults.setValue("Vacation", forKey: UserDefaults.Security.SecurityMode)
-                default: break
+        //FIXME: Pucalo je security zato sto nema u svim gatewayovima security
+        if securities.count != 0 {
+            let address = [Byte(Int(securities[0].addressOne)), Byte(Int(securities[0].addressTwo)), Byte(Int(securities[0].addressThree))]
+            if byteArray[2] == address[0] && byteArray[3] == address[1] && byteArray[4] == address[2] {
+                let defaults = NSUserDefaults.standardUserDefaults()
+                if byteArray[7] == 0x02 {
+                    switch byteArray[8] {
+                    case 0x00:
+                        defaults.setValue("Disarm", forKey: UserDefaults.Security.SecurityMode)
+                    case 0x01:
+                        defaults.setValue("Away", forKey: UserDefaults.Security.SecurityMode)
+                    case 0x02:
+                        defaults.setValue("Night", forKey: UserDefaults.Security.SecurityMode)
+                    case 0x03:
+                        defaults.setValue("Day", forKey: UserDefaults.Security.SecurityMode)
+                    case 0x04:
+                        defaults.setValue("Vacation", forKey: UserDefaults.Security.SecurityMode)
+                    default: break
+                    }
                 }
-            }
-            if byteArray[7] == 0x03 {
-                switch byteArray[8] {
-                case 0x00:
-                    defaults.setValue("Idle", forKey: UserDefaults.Security.AlarmState)
-                case 0x01:
-                    defaults.setValue("Trouble", forKey: UserDefaults.Security.AlarmState)
-                case 0x02:
-                    defaults.setValue("Alert", forKey: UserDefaults.Security.AlarmState)
-                case 0x03:
-                    defaults.setValue("Alarm", forKey: UserDefaults.Security.AlarmState)
-                default: break
+                if byteArray[7] == 0x03 {
+                    switch byteArray[8] {
+                    case 0x00:
+                        defaults.setValue("Idle", forKey: UserDefaults.Security.AlarmState)
+                    case 0x01:
+                        defaults.setValue("Trouble", forKey: UserDefaults.Security.AlarmState)
+                    case 0x02:
+                        defaults.setValue("Alert", forKey: UserDefaults.Security.AlarmState)
+                    case 0x03:
+                        defaults.setValue("Alarm", forKey: UserDefaults.Security.AlarmState)
+                    default: break
+                    }
                 }
-            }
-            if byteArray[7] == 0x04 {
-                switch byteArray[8] {
-                case 0x00:
-                    defaults.setBool(false, forKey: UserDefaults.Security.IsPanic)
-                case 0x01:
-                    defaults.setBool(true, forKey: UserDefaults.Security.IsPanic)
-                default: break
+                if byteArray[7] == 0x04 {
+                    switch byteArray[8] {
+                    case 0x00:
+                        defaults.setBool(false, forKey: UserDefaults.Security.IsPanic)
+                    case 0x01:
+                        defaults.setBool(true, forKey: UserDefaults.Security.IsPanic)
+                    default: break
+                    }
                 }
+                print("EHGSecuritySeczurityMode - \(defaults.valueForKey(UserDefaults.Security.SecurityMode)) *** EHGSecurityAlarmState - \(defaults.valueForKey(UserDefaults.Security.AlarmState)) *** EHGSecurityPanic - \(defaults.boolForKey(UserDefaults.Security.IsPanic))")
+                
+                NSNotificationCenter.defaultCenter().postNotificationName(NotificationKey.RefreshSecurity, object: self, userInfo: nil)
             }
-            print("EHGSecuritySeczurityMode - \(defaults.valueForKey(UserDefaults.Security.SecurityMode)) *** EHGSecurityAlarmState - \(defaults.valueForKey(UserDefaults.Security.AlarmState)) *** EHGSecurityPanic - \(defaults.boolForKey(UserDefaults.Security.IsPanic))")
-            
-            NSNotificationCenter.defaultCenter().postNotificationName(NotificationKey.RefreshSecurity, object: self, userInfo: nil)
         }
     }
     
