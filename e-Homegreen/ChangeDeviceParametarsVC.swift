73c063c9ecbeba4016e2741a4abf8b663656a6f4
Vladimir Zivanov <vrza88@gmail.com>
2016-09-09 15:27:09 +0200

    device parametar fixed zones and categories

--- e-Homegreen/ChangeDeviceParametarsVC.swift
+++ e-Homegreen/ChangeDeviceParametarsVC.swift
@@ -72,23 +72,24 @@ class ChangeDeviceParametarsVC: PopoverVC {
         txtFieldName.text = device.name
         lblAddress.text = "\(returnThreeCharactersForByte(Int(device.gateway.addressOne))):\(returnThreeCharactersForByte(Int(device.gateway.addressTwo))):\(returnThreeCharactersForByte(Int(device.address)))"
         lblChannel.text = "\(device.channel)"
-        level = DatabaseHandler.sharedInstance.returnZoneWithId(Int(device.parentZoneId), location: device.gateway.location)
+        
+        level = DatabaseZoneController.shared.getZoneById(Int(device.parentZoneId), location: device.gateway.location)
         if level != nil{
             btnLevel.setTitle(level!.name, forState: UIControlState.Normal)
         }else{
             btnLevel.setTitle("All", forState: UIControlState.Normal)
         }
         
-        zoneSelected = DatabaseHandler.sharedInstance.returnZoneWithId(Int(device.zoneId), location: device.gateway.location)
+        zoneSelected = DatabaseZoneController.shared.getZoneById(Int(device.zoneId), location: device.gateway.location)
         if zoneSelected != nil{
             btnZone.setTitle(zoneSelected!.name, forState: UIControlState.Normal)
         }else{
             btnZone.setTitle("All", forState: UIControlState.Normal)
         }
         
-        let category = DatabaseHandler.sharedInstance.returnCategoryWithId(Int(device.categoryId), location: device.gateway.location)
+        let category = DatabaseCategoryController.shared.getCategoryById(Int(device.categoryId), location: device.gateway.location)
         if category != ""{
-            btnCategory.setTitle(category, forState: UIControlState.Normal)
+            btnCategory.setTitle(category?.name, forState: UIControlState.Normal)
         }else{
            btnCategory.setTitle("All", forState: UIControlState.Normal)
         }
